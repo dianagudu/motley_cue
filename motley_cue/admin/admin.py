954aa158b090a3e0bab6df8287aa104f53ce544c
Diana Gudu <diana.gudu@kit.edu>
2021-02-08 17:39:14 +0100

    removed possibility for admin users to undeploy accounts

--- motley_cue/admin/admin.py
+++ motley_cue/admin/admin.py
@@ -13,17 +13,10 @@ async def read_root(request: Request):
         "description": "This is the admin API for mapping remote identities to local identities.",
         "usage": "All endpoints are available using a bearer token and need subject and issuer of account to be modified, via 'sub' and 'iss' variables.",
         "endpoints": {
-            "/undeploy": "Deprovision a local account.",
             "/suspend": "Suspends a local account.",
             "/resume": "Restores a suspended local account."
         }
     }
-
-
-@api.get("/undeploy")
-@mapper.authorised_admin_required()
-async def undeploy(request: Request, sub: str, iss: str):
-    return mapper.admin_action(sub, iss, AdminActions.undeploy)
 
 
 @api.get("/suspend")
