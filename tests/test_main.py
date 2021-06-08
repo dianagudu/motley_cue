81f5329a2f73a1fc1b934fce8a65de17a5a822b3
Diana Gudu <diana.gudu@kit.edu>
2021-06-08 22:40:36 +0200

    wip testing: passing AT through env var

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,11 +1,11 @@
-def test_root(test_api):
+from .utils import *
+
+
+def test_public_endpoints(test_api):
     response = test_api.get("/")
     assert response.status_code == 200
-    assert set(response.json().keys()) == set(
-        ["description", "endpoints", "usage"])
-    assert set(response.json()["endpoints"].keys()) == set(
-        ["/info", "/info/authorisation", "/user", "/admin", "/verify_user"]
-    )
+    assert set(response.json().keys()) == set(ROOT_KEYS)
+    assert set(response.json()["endpoints"].keys()) == set(ROOT_ENDPOINTS)
 
     response = test_api.get("/user")
     assert response.status_code == 200
@@ -19,9 +19,36 @@ def test_root(test_api):
 
     response = test_api.get("/info")
     assert response.status_code == 200
-    assert set(response.json().keys()) == set(["login info", "supported OPs"])
+    assert set(response.json().keys()) == set(INFO_KEYS)
 
 
-def test_info_authorisation(test_api):
+def test_protected_endpoints_forbidden(test_api):
     response = test_api.get("/info/authorisation")
     assert response.status_code == 403
+
+    response = test_api.get("/verify_user")
+    assert response.status_code == 403
+
+    for endpoint in USER_ENDPOINTS:
+        response = test_api.get(f"/user{endpoint}")
+        assert response.status_code == 403
+
+    for endpoint in ADMIN_ENDPOINTS:
+        response = test_api.get(f"/admin{endpoint}")
+        assert response.status_code == 403
+
+
+def test_bad_token(test_api):
+    token = "BADTOKEN"
+    headers = {"Authorization": f"Bearer {token}"}
+    response = test_api.get("/info/authorisation", headers=headers)
+    assert response.status_code == 401
+
+
+def test_login_info_authorisation(test_api_with_token):
+    test_api, token = test_api_with_token
+    headers = {"Authorization": f"Bearer {token}"}
+    response = test_api.get("/info/authorisation", headers=headers)
+    assert response.status_code == 200
+    assert set(response.json().keys()).issuperset(
+        set(INFO_AUTHZ_KEYS))
