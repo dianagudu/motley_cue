160c296816b805ff4654761dc24434561f769845
Diana Gudu <diana.gudu@kit.edu>
2021-07-06 16:41:07 +0200

    improve logging

--- motley_cue/mapper/__init__.py
+++ motley_cue/mapper/__init__.py
@@ -10,6 +10,7 @@ from fastapi.security import HTTPBearer
 from .config import Config
 from .authorisation import Authorisation
 from .local_user_management import LocalUserManager
+from .utils import MapperResponse
 
 
 class Mapper:
@@ -61,13 +62,13 @@ class Mapper:
 
     def info(self):
         # here we return service name, description, supported OPs
-        return {
+        return MapperResponse({
             "login info": self.__lum.login_info(),
             "supported OPs": self.__authorisation.trusted_op_list,
-        }
+        })
 
     def info_authorisation(self, request):
-        return self.__authorisation.info(request)
+        return MapperResponse(self.__authorisation.info(request))
 
     def login_required(self):
         return self.__authorisation.login_required()
@@ -80,31 +81,22 @@ class Mapper:
 
     def deploy(self, request: Request):
         userinfo = self.__authorisation.get_userinfo_from_request(request)
-        return self.__lum.deploy(userinfo)
+        return MapperResponse(**self.__lum.deploy(userinfo))
 
     def get_status(self, request: Request):
         userinfo = self.__authorisation.get_uid_from_request(request)
-        return self.__lum.get_status(userinfo)
+        return MapperResponse(**self.__lum.get_status(userinfo))
 
     def suspend(self, request: Request):
         userinfo = self.__authorisation.get_uid_from_request(request)
-        return self.__lum.suspend(userinfo)
+        return MapperResponse(**self.__lum.suspend(userinfo))
 
     def admin_suspend(self, sub: str, iss: str):
-        return self.__lum.admin_suspend(sub, iss)
+        return MapperResponse(**self.__lum.admin_suspend(sub, iss))
 
     def admin_resume(self, sub: str, iss: str):
-        return self.__lum.admin_resume(sub, iss)
+        return MapperResponse(**self.__lum.admin_resume(sub, iss))
 
     def verify_user(self, request: Request, username: str):
-        state = "unknown"
-        try:
-            result = self.get_status(request)
-            state = result['state']
-            local_username = result['message'].split()[1]
-        except Exception:
-            local_username = None
-        return {
-            "state": state,
-            "verified": (local_username == username)
-        }
+        userinfo = self.__authorisation.get_uid_from_request(request)
+        return MapperResponse(self.__lum.verify_user(userinfo, username))
--- motley_cue/mapper/local_user_management.py
+++ motley_cue/mapper/local_user_management.py
@@ -4,7 +4,7 @@ from enum import Enum
 from feudal_globalconfig import globalconfig
 globalconfig.config['parse_commandline_args'] = False
 from ldf_adapter.config import CONFIG as LDF_ADAPTER_CONFIG
-from ldf_adapter.results import ExceptionalResult
+from ldf_adapter.results import ExceptionalResult, Rejection
 from ldf_adapter import User
 
 
@@ -55,10 +55,31 @@ class LocalUserManager():
             login_info = {}
         return login_info
 
+    def verify_user(self, userinfo, username):
+        state = "unknown"
+        local_username = None
+        try:
+            logging.getLogger(__name__).info(
+                f"Attempting to verify if local username for userinfo {userinfo} is '{username}'")
+            result = self.get_status(userinfo)
+            if result["status_code"] == 200:
+                state = result["content"]["state"]
+                if state != States.not_deployed.name and state != States.unknown.name:
+                    local_username = result["content"]["message"].split()[1]
+        except Exception:
+            logging.getLogger(__name__).warning(f"Could not verify token for username {username}: {e}")
+        return {
+            "state": state,
+            "verified": (local_username == username)
+        }
+
     def _reach_state(self, userinfo, state_target: States):
         if userinfo is None:
             logging.getLogger(__name__).error("Cannot process null input")
-            return None
+            return {
+                "content": None,
+                "status_code": 500
+            }
         # build input for feudalAdapter
         data = {
             "state_target": state_target.name,
@@ -67,19 +88,27 @@ class LocalUserManager():
             },
         }
         try:
-            logging.getLogger(__name__).debug(
-                f"Attempting to reach state '{state_target.name}'")
             result = User(data).reach_state(data['state_target'])
         except ExceptionalResult as result:
+            if isinstance(result, Rejection):
+                status_code = 403  # forbidden
+            else:
+                status_code = 500  # internal server error
             result = result.attributes
-            logging.getLogger(__name__).debug(
+            logging.getLogger(__name__).warning(
                 "Reached state '{state}': {message}".format(**result))
-            return result
+            return {
+                "content": result,
+                "status_code": status_code
+            }
         else:
             result = result.attributes
-            logging.getLogger(__name__).debug(
+            logging.getLogger(__name__).info(
                 "Reached state '{state}': {message}".format(**result))
-            return result
+            return {
+                "content": result,
+                "status_code": 200
+            }
 
     def _admin_action(self, sub: str, iss: str, action: AdminActions):
         data = {
@@ -91,6 +120,9 @@ class LocalUserManager():
             }
         }
         try:
+            logging.getLogger(__name__).info(
+                f"Attempting to perform admin action '{action.name}' on user: [sub: {sub}, iss: {iss}]"
+            )
             if action == AdminActions.suspend:
                 result = User(data).reach_state(States.suspended.name)
             elif action == AdminActions.resume:
@@ -98,12 +130,22 @@ class LocalUserManager():
             elif action == AdminActions.undeploy:
                 result = User(data).reach_state(States.not_deployed.name)
         except ExceptionalResult as result:
+            if isinstance(result, Rejection):
+                status_code = 403  # forbidden
+            else:
+                status_code = 500  # internal server error
             result = result.attributes
-            logging.getLogger(__name__).debug(
+            logging.getLogger(__name__).warning(
                 "Reached state '{state}': {message}".format(**result))
-            return result
+            return {
+                "content": result,
+                "status_code": status_code
+            }
         else:
             result = result.attributes
-            logging.getLogger(__name__).debug(
+            logging.getLogger(__name__).info(
                 "Reached state '{state}': {message}".format(**result))
-            return result
+            return {
+                "content": result,
+                "status_code": 200
+            }
