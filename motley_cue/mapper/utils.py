4abe7c468376d1d34dec15e9c3539113cd5a4724
Diana Gudu <diana.gudu@kit.edu>
2021-07-15 15:13:15 +0200

    fix given iss for admin actions

--- motley_cue/mapper/utils.py
+++ motley_cue/mapper/utils.py
@@ -1,5 +1,13 @@
 from fastapi.responses import JSONResponse
 
+
 class MapperResponse(JSONResponse):
     def __init__(self, content, status_code=200):
         super().__init__(content, status_code)
+
+# store here the exact URL for some OPs as found in an AT issued by them
+# e.g. some have a trailing slash, but flaat strips that off
+EXACT_OP_URLS = [
+    'https://wlcg.cloud.cnaf.infn.it/',
+    'https://aai.egi.eu/oidc/',
+]
