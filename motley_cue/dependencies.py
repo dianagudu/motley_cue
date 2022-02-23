730cb50f107db7fa2588b1967196c45d8551ecbf
Diana Gudu <diana.gudu@kit.edu>
2022-02-24 00:25:21 +0100

    add validator for docs url settings

--- motley_cue/dependencies.py
+++ motley_cue/dependencies.py
@@ -1,5 +1,5 @@
 from typing import Optional
-from pydantic import BaseSettings
+from pydantic import BaseSettings, validator
 
 from .version import __version__
 from .mapper import Mapper, Config
@@ -12,6 +12,18 @@ class Settings(BaseSettings):
     openapi_url: str = "/openapi.json"
     docs_url: Optional[str] = None
     redoc_url: Optional[str] = None
+
+    @validator("openapi_url", allow_reuse=True)
+    def must_start_with_slash(cls, url):
+        if not url.startswith("/"):
+            raise ValueError("Routed paths must start with '/'")
+        return url
+
+    @validator("docs_url", "redoc_url", allow_reuse=True)
+    def must_start_with_slash_or_none(cls, url):
+        if url is not None and not url.startswith("/"):
+            raise ValueError("Routed paths must start with '/'")
+        return url
 
 
 mapper = Mapper(Config.from_files([]))
