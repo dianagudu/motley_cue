b8ac7006f285eaf55c37953f6bcfe8c41e8036ba
Marcus <marcus@hardt-it.de>
2024-10-30 18:51:45 +0100

    add more debug

--- .gitlab-ci-scripts/local-before-script.sh
+++ .gitlab-ci-scripts/local-before-script.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 
+echo "### local-before-script.sh #####################################"
 echo -n "python3 version: "
 python3 --version
+ls -l `which python3`
+ls -l /etc/alternatives/python3*
+ls -l /usr/bin/python3*
+
 # # Tweak python versions for some distros:
 #
 # case "${DISTRO}-${RELEASE}" in
@@ -10,3 +15,4 @@ python3 --version
 #             echo "setting python3.11 as a default for python3"
 #             cd /etc/alternatives
 #             rm python3
+echo "END local-before-script.sh #####################################"
